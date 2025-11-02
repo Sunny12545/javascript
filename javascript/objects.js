77e41c580d7c7e232b50b37d0d3eee13b82328ef
Sunny12545 <thallapellisunny684@gmail.com>
2025-11-02 07:03:59 +0000

    object methods completed

--- javascript/objects.js
+++ javascript/objects.js
@@ -17,3 +17,15 @@ console.log(typeof myobj[mysym]);
 //changing data in objects 
 myobj.email = "sunny125@gmail.com";
 console.log(myobj.email)
+
+//combining objects 
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+const obj3 = {5:"a",6:"b"}
+
+const obj4 = {...obj1, ...obj2, ...obj3}
+console.log(obj4);
+console.log(Object.keys(obj4));
+console.log(Object.values(obj4));
+console.log(Object.entries(obj4));
