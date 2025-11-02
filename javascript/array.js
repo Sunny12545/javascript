6ee0a97fe63c0877987c1a02e233f2fc110cbd35
Sunny12545 <thallapellisunny684@gmail.com>
2025-11-02 06:38:26 +0000

    array code completed

--- javascript/array.js
+++ javascript/array.js
@@ -1,5 +1,5 @@
 //array
-
+/*
 const myarray = [0,1,2,3,4,5,6];
 console.log(myarray[0]);
 
@@ -22,5 +22,22 @@ console.log(myarray);
 
 const arr2 = myarray.splice(1,3)
 console.log(arr2);
-console.log(myarray);
+console.log(myarray);*/
 
+// part two arrays
+
+const marvelheros = ["thor","spiderman","ironman"];
+const dcheros = ["superman","batman","flash"];
+
+// marvelheros.push(dcheros)
+
+// console.log(marvelheros);
+// marvelheros.concat(dcheros);
+// console.log(marvelheros)
+
+const allnewheros = [...marvelheros, ...dcheros]
+console.log(allnewheros);
+
+const anotherarray = [1,2,3,4,[5,6,7],8,[9,10,[11,12]]];
+const realarray = anotherarray.flat(Infinity)
+console.log(realarray)
