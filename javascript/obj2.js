825063acf6b6805e74e014e60a26aaf23d0fa83b
Sunny12545 <thallapellisunny684@gmail.com>
2025-11-03 04:30:30 +0000

    objects complted

---
+++ javascript/obj2.js
@@ -0,0 +1,23 @@
+const course = {
+    coursename : "javascript",
+    price : 1000,
+    courseintructor : "sunny"
+}
+//objects destructuring
+
+const {courseintructor: instructor} = course
+// console.log(courseintructor)
+console.log(instructor);
+
+//apis objects 
+
+// {
+//     "name": "sunny",
+//     "coursename" : "javascript",
+//     "price" : "free"
+// }
+
+//apis arrays 
+[
+    
+]
